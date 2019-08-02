07d9ebd4d17761a7a441d79931ec59b0adb70ad9
Xerams <chris.cybula@gmail.com>
2019-08-02 17:15:23 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <h1>react</h1>,
+    destination
+);

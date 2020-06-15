4ca4023c277ecd4a82af2cc8cc5bb975bc8deac3
chris-cybula <chris.cybula@gmail.com>
2020-06-15 11:24:35 +0100

    gatsby-config

---
+++ client/src/components/App.js
@@ -0,0 +1,52 @@
+import React from "react"
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+function App() {
+  const [item, setItem] = useState()
+  const [listItems, setListItem] = useState([])
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = async () => {
+    const res = await axios.get("http://localhost:3000")
+    const list = res.data.map(a => a.description)
+
+    setListItem(list)
+  }
+
+  const addItem = async () => {
+    await axios.post("http://localhost:3000", {
+      description: item,
+    })
+
+    setListItem([...listItems, item])
+  }
+
+  const deleteItem = async e => {
+    const removedItem = e.target.parentElement.firstChild.innerHTML
+    await axios.delete(`http://localhost:3000/${removedItem}`)
+
+    setListItem(listItems.filter(e => e !== removedItem))
+  }
+
+  return (
+    <div>
+      <input onChange={event => setItem(event.target.value)} />
+      <button onClick={addItem}>Add</button>
+
+      {listItems.map((item, i) => {
+        return (
+          <div key={i} className="itemWrapper">
+            <li>{item}</li>
+            <button onClick={deleteItem}>Delete</button>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default App
--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import "./index.css";
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.js";
-
-var destination = document.querySelector("#container");
-
-ReactDOM.render(<App />, destination);

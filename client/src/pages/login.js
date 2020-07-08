ece62e6b0d9a919e29bbdd47aa7b56d167586171
chris-cybula <chris.cybula@gmail.com>
2020-07-08 16:56:27 +0100

    Add register

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,28 +1,42 @@
 import React from "react"
 import { Link } from "gatsby"
 import { useState } from "react"
-
+import axios from "axios"
 
 const Login = () => {
   const [registerData, setRegisterData] = useState(
     { 
-      "name": "",
-      "email": "",
-      "password": "",
+      name: "",
+      email: "",
+      password: "",
     }
   )
 
-  console.log(registerData)
+  // const handleRegister = async () => {
+  //   console.log(JSON.stringify(registerData))
+
+  //   await axios.post('http://localhost:3000/api/user/register', data: registerData)
+  // }
+
+  const handleRegister = async () => {
+  await axios({
+    method: 'post',
+    url: 'http://localhost:3000/api/user/register',
+    data: registerData
+  })
+
+  alert('Register')
+}
 
   return (
     <>
     <h1>Login</h1>
     <div>
        <p>Register</p>
-        <input placeholder="username" onChange={e => setRegisterData(e.target.value)}/>
-        <input placeholder="email"/>
-        <input placeholder="password"/>
-        <button>Register</button> 
+        <input placeholder="username" onChange={e => setRegisterData({...registerData, name: e.target.value})}/>
+        <input placeholder="email" onChange={e => setRegisterData({...registerData, email: e.target.value})}/>
+        <input placeholder="password" onChange={e => setRegisterData({...registerData, password: e.target.value})}/>
+        <button onClick={handleRegister}>Register</button> 
     </div>
     <div>
        <p>Login</p>
@@ -37,9 +51,4 @@ const Login = () => {
 
 export default Login
 
-
-function useInput({ type /*...*/ }) {
-  const [value, setValue] = useState("");
-  const input = <input value={value} onChange={e => setValue(e.target.value)} type={type} />;
-  return [value, input];
-}
+ 

aedd7ed3713162ceb17f7b929fc6735835a7e476
chris-cybula <chris.cybula@gmail.com>
2020-07-14 15:19:46 +0100

    Code refactoring

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,23 +2,8 @@ import React from "react"
 import App from "../components/App.js"
 import Layout from "../components/Layout.js"
 import { navigate } from "gatsby"
-import { createStore } from "redux"
-import allReducers from '../reducers'
-import { getToken } from "../actions/getToken";
+import { store } from "../store"
 import { Provider } from "react-redux";
-
-//store
-let store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-
-//console
-store.subscribe(() => console.log(store.getState()))
-
-//dispatch
-store.dispatch(getToken())
-
 
 const Index = () => {
   const isLoggedIn = true; 

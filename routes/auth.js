d3c0d71f132bddb22ba2bf1aaf52ed0d7c689884
chris-cybula <chris.cybula@gmail.com>
2020-08-03 15:23:07 +0100

    Add name validation

--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,9 @@ router.post("/register", async (req, res) => {
   const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send("Email already exists");
 
+  const nameExist = await User.findOne({ name: req.body.name });
+  if (nameExist) return res.status(400).send("Name already exists");
+
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
--- routes/settings.js
+++ routes/settings.js
@@ -5,6 +5,9 @@ const { changeNameValidation, changeEmailValidation, changePasswordValidation } 
 const bcrypt = require("bcryptjs");
 
 router.post('/name', verify, async (req, res) => {
+    const nameExist = await User.findOne({ name: req.body[1].newName });
+    if (nameExist) return res.status(400).send("Name already exists");
+  
     const { error } = changeNameValidation({name: req.body[1].newName});
     if (error) return res.status(400).send(error.details[0].message);
 

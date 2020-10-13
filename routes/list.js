e59d47c97d69877ee1da145be2472f11af62a916
chris-cybula <chris.cybula@gmail.com>
2020-10-13 22:08:08 +0100

    Code refactoring

--- routes/list.js
+++ routes/list.js
@@ -11,7 +11,7 @@ router.get("/", verify, async (req, res) => {
     const data = await User.find({ _id: req.user });
     res.json(data);
   } catch (err) {
-    res.json(err);
+    res.json('error');
   }
 });
 
@@ -41,36 +41,12 @@ router.post("/", verify, async (req, res) => {
   }
 });
 
-//delete
 router.delete("/:description", verify, async (req, res) => {
   try {
     User.findByIdAndUpdate(req.user, {
       $pull: { data: req.params.description },
     }).exec();
     res.json("ok");
-  } catch (err) {
-    res.json(err);
-  }
-});
-
-//get specific item
-router.get("/:description", async (req, res) => {
-  try {
-    const item = await Item.find({ description: req.params.description });
-    res.json(item);
-  } catch (err) {
-    res.json(err);
-  }
-});
-
-//update
-router.patch("/:description", async (req, res) => {
-  try {
-    const updatedItem = await Item.updateOne(
-      { description: req.params.description },
-      { $set: { description: req.body.description } }
-    );
-    res.json(updatedItem);
   } catch (err) {
     res.json(err);
   }
